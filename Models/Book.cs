9137b566b71ed1ee436c1ac7c709af5ae8a802ae
Praneeth <developer.praneethrk@gmail.com>
2026-02-13 13:54:25 +0530

    Added Pop Up Functionality

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,4 +9,5 @@ public class Book
     public int PublicationYear { get; set; }
     public string ISBN { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
 }
